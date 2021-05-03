d94275391aa8962e11e80332f5cba6d25dce8146
sanyatop1 <71902164+sanyatop1@users.noreply.github.com>
2021-05-03 14:55:06 +0300

    Add files via upload

---
+++ ProgbaseLab.ImageEditor.Common/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ProgbaseLab.ImageEditor;
+using System.Drawing;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+
+namespace ProgbaseLab.ImageEditor.Common
+{
+    public interface IRunnable
+
+    {
+        
+        void Crop(int height, int width, int top, int left,string filename,string fileout);
+          void RotateLeft90(string filename,string fileout);
+          void ExtractRed(string filename,string fileout);
+          void Grayscale(string filename,string fileout);
+          void ChangeSaturation(string filename,string fileout,int saturation);
+        
+    }
+    
+}
